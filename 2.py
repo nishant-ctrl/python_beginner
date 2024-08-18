2fc27538db604b697dcefb44a26d1e21df00b4fb
nishant-ctrl <knishant9135@gmail.com>
2024-08-18 14:36:56 +0530

    add second file

---
+++ 2.py
@@ -0,0 +1,17 @@
+name="Nishant Kumar"  #double quotes
+name1='my_program'    #single quotes
+print("Hello",name)   #comma leaves a space
+print("Hello"+name1)  # + doesnot gives space
+
+apple="He wants to eat an \"apple\""  #use of \" to print quotes
+print(apple)
+fruit='He wants to eat an "apple"'    #use single quote instead od double quote
+print(fruit)
+
+#Multi line string  use '''_''' OR """_"""
+str="""Hello World    
+How are you
+Good"""
+print(str)
+
+
