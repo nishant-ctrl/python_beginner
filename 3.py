56d4490f0bd59248ed535190044b70f3bbd9c267
nishant-ctrl <knishant9135@gmail.com>
2024-08-19 22:52:27 +0530

    Add new file no. 3

---
+++ 3.py
@@ -0,0 +1,14 @@
+a=int(input("Enter your age: "))
+if(a>=18):
+    print("You can drive.")
+else:
+    print("You can't drive")
+
+    
+num=int(input("Enter the number: "))
+if(num>0):
+    print("Positive")
+elif(num==0):
+    print("Zero")     
+else:
+    print("Negative")           
