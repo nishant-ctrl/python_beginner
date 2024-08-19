714e4631f9f7a24002b2436fc7bf56a4f91fda6a
nishant-ctrl <knishant9135@gmail.com>
2024-08-20 00:19:35 +0530

    changes in 4 file

--- 4.py
+++ 4.py
@@ -1,28 +1,28 @@
-# print("Enter time in 24 hours format: ")
-# h=int(input("Enter hours: "))
-# m=int(input("Enter minutes: "))
-# s=int(input("Enter seconds: "))
-# if(h<12 & h>=0):
-#     print("Good Morning")
-# elif(h>=12 & h<18):
-#     print("Good Afternoon")
-# elif(h>=18 & h<=24):
-#     print("Good Night")
-# else:
-#     print("Wrong Data")
+print("Enter time in 24 hours format: ")
+h=int(input("Enter hours: "))
+m=int(input("Enter minutes: "))
+s=int(input("Enter seconds: "))
+if(h<12 and h>=0):
+    print("Good Morning")
+elif(h>=12 and h<18):
+    print("Good Afternoon")
+elif(h>=18 and h<=24):
+    print("Good Night")
+else:
+    print("Wrong Data")
 
-# import time
-# timestamp = time.strftime('%H:%M:%S')
-# hour = int(time.strftime('%H'))
-# print(timestamp)
-# if(hour>=5 & hour<12):
-#     print("Good Morning")
-# elif(hour>=12 & hour<17):
-#     print("Good Afternoon")
-# elif(hour>=17 & hour<20):
-#     print("Good Evening")
-# elif((hour>=20 & hour<=24) | (hour>=0 & hour<5)):
-#     print("Good Night")            
+import time
+timestamp = time.strftime('%H:%M:%S')
+hour = int(time.strftime('%H'))
+print(timestamp)
+if(hour>=5 and hour<12):
+    print("Good Morning")
+elif(hour>=12 and hour<17):
+    print("Good Afternoon")
+elif(hour>=17 and hour<20):
+    print("Good Evening")
+elif((hour>=20 and hour<=24) | (hour>=0 & hour<5)):
+    print("Good Night")            
 
 x=int(input("Enter value of x: "))
 match x:
