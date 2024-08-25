ed9c8c3ded9246b3ebddbab746a8ddb88ae653ee
nishant-ctrl <knishant9135@gmail.com>
2024-08-25 16:27:35 +0530

    update

--- 16.py
+++ 16.py
@@ -5,5 +5,13 @@ if(a<10 or a>20):
     raise ValueError("Number should be in b/w 10 and 20")
 
 # we can raise custom error with classes
-
+i=input("Enter no b/w 10 and 20: ")
+if(i=="quit"):
+    exit()
+elif(i.isalpha()):
+    print("Please Enter an integer value.")
+elif(int(i)<10 or int(i)>20):
+    raise ValueError("Number should be in b/w 10 and 20")
+else:
+    print("Input is accepted.")        
 
