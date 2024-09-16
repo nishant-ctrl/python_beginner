582b7e66937b48e37e8a021fa4347658d17f0478
nishant-ctrl <knishant9135@gmail.com>
2024-09-17 00:26:21 +0530

    add getter and setter file

--- 30.py
+++ 30.py
@@ -1,2 +1,22 @@
 # getters and setters
 
+class MyClass:
+  def __init__(self, value):
+      self._value = value
+    
+  def show(self):
+    print(f"Value is {self._value}")
+    
+  @property
+  def new_value(self):
+      return self._value
+    
+  @new_value.setter
+  def new_value(self, new_value):
+      self._value = new_value
+
+obj = MyClass(10)
+print(obj.new_value)
+obj.new_value = 67
+print(obj.new_value)
+obj.show()
